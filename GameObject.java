54912d0e92216ad77c0e2fb99c2836f8e13f1ef2
McArok <72186634+mcarok@users.noreply.github.com>
2020-10-15 23:34:35 +0300

    Add files via upload

---
+++ GameObject.java
@@ -0,0 +1,8 @@
+public class GameObject {
+    public static void main(String[] args) {
+            }
+    public int x,y;
+    public void gameObject(int x, int y){
+        this.x=x; this.y = y;
+    }
+}
